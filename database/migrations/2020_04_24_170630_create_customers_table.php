a746e48dd92635aa8450bdded170196c53fbfca7
Sahan Sachintha <sahansachintha.dev@gmail.com>
2026-05-14 14:22:18 +0530

    Fix composer dependencies

--- database/migrations/2020_04_24_170630_create_customers_table.php
+++ database/migrations/2020_04_24_170630_create_customers_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
-            $table->string('avatar')->nullable();
+            $table->string('vehicle_identifier')->nullable();
             $table->timestamps();
         });
     }
